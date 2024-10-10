f715eed1f25c5623518719e9f02fd72120ff0b7e
InesParis <inesparisarranz@gmail.com>
2024-10-10 21:36:57 +0200

    linking back end to front end
    linking back-end to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import "./index.css";
 import ListHeader from "./components/ListHeader";
+import { useEffect } from "react";
 const App = () => {
+  const getData = async () => {
+    const userEmail = "inesparisarranz@gmail.com";
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="app">
       <ListHeader listName={"👩🏻‍💻 Projects tick list"} />
     </div>
   );
 };
-
+useEffect(() => getData, []);
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const PORT = process.env.PORT ?? 8000;
-const express = requiere("express");
+const express = require("express");
+const cors = requiere("cors");
 const app = express();
+const pool = require("./db.js");
+
+app.use(cors());
 //get all todos
 app.get("/todos", async (req, res) => {
+  const userEmail = req.params;
+
   try {
-    //await
+    const todos = await pool.query("SELECT * FROM todos WHERE user_email= $1", [
+      userEmail,
+    ]);
+    res.json(todos.rows);
   } catch (err) {
     console.error(error);
   }
