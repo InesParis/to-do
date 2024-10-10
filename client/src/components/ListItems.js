1b4907a2f46e348b0d9c9b637579972d0773219b
InesParis <inesparisarranz@gmail.com>
2024-10-10 22:14:03 +0200

    ListItems.js
    building the front end for the list items

--- client/src/components/ListItems.js
+++ client/src/components/ListItems.js
@@ -1,7 +1,14 @@
+import TickIcon from "./TickIcon";
+import ProgressBar from "./ProgressBar";
+
 const ListItems = ({ task }) => {
   return (
     <div className="list-item">
-      <p>{task.title}</p>
+      <div className="infor-container">
+        <TickIcon />
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
+      </div>
     </div>
   );
 };
