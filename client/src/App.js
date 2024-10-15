d65f2c92d531e158e7661da6fae33afd3305645a
InesParis <inesparisarranz@gmail.com>
2024-10-15 17:48:18 +0200

    Working on cookies and login and sign in
    working on signin and login incorporating cookies.

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import ListItems from "./components/ListItems";
 import ListHeader from "./components/ListHeader";
 import Auth from "./components/Auth";
 import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 const App = () => {
-  const userEmail = "inesparisarranz@gmail.com";
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const authToken = cookies.AuthToken;
+  const userEmail = cookies.Email;
   const [tasks, setTasks] = useState(null);
-
-  const authToken = false;
 
   const getData = async () => {
     try {
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { useCookies } from "react-cookie";
 
 const Auth = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
   const [isLogIn, setIsLogin] = useState(true);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+
+  console.log(cookies);
 
   const viewLogin = (status) => {
     setError(null);
@@ -18,7 +22,20 @@ const Auth = () => {
       setError("Make sure passwords match!");
       return;
     }
-    await fetch(`http://localhost:8000/${endpoint}`);
+    const response = await fetch(`http://localhost:8000/${endpoint}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    if (data.detail) {
+      setError(data.detail);
+    } else {
+      setCookie("Email", data.email);
+      setCookie("AuthToken", data.token);
+
+      window.location.reload();
+    }
   };
 
   return (
@@ -26,9 +43,23 @@ const Auth = () => {
       <div className="auth-container-box">
         <form>
           <h2>{isLogIn ? "Please log in" : "Please sign up!"}</h2>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          {!isLogIn && <input type="password" placeholder="confirm password" />}
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {!isLogIn && (
+            <input
+              type="password"
+              placeholder="confirm password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          )}
           <input
             type="submit"
             className="create"
